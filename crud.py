6738fe7235ef3236b58b477eaa5373e9b5bd889c
AJ Glodowski <ajglodo@gmail.com>
2022-12-05 15:15:23 -0500

    Stage 2 Demo

--- crud.py
+++ crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 import models, schemas
 
@@ -13,3 +14,22 @@ def create_player(db: Session, player: schemas.PlayerBase):
     db.commit()
     db.refresh(db_player)
     return db_player
+
+def update_team(db: Session, team_id: int, team: schemas.TeamBase):
+    db_team = db.query(models.Team).filter(models.Team.team_id == team_id).first()
+    if not db_team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    hero_data = team.dict(exclude_unset=True)
+    for key, value in hero_data.items():
+        setattr(db_team, key, value)
+    db.add(db_team)
+    db.commit()
+    db.refresh(db_team)
+    return db_team
+
+def create_team(db: Session, team: schemas.TeamBase):
+    db_team = models.Team(**team.dict())
+    db.add(db_team)
+    db.commit()
+    db.refresh(db_team)
+    return db_team
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
 import crud, models, schemas
 from database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,6 +16,16 @@ def get_db():
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/create_player")
 def create_player(player: schemas.PlayerBase, db: Session=Depends(get_db)):
     return crud.create_player(db=db, player=player)
@@ -22,3 +34,21 @@ def create_player(player: schemas.PlayerBase, db: Session=Depends(get_db)):
 def get_players(db: Session=Depends(get_db)):
     result = db.execute('SELECT * FROM players')
     return result.all()
+
+@app.get("/team/{teamId}")
+def get_team(teamId: str, db: Session=Depends(get_db)):
+    result = db.execute('SELECT * FROM teams WHERE team_id = '+teamId)
+    return result.all()
+
+@app.get("/league/{leagueId}")
+def get_league(leagueId: str, db: Session=Depends(get_db)):
+    result = db.execute('SELECT * FROM leagues WHERE league_id = '+leagueId)
+    return result.all()
+
+@app.put("/updateTeam/{teamId}")
+def update_team(team: schemas.TeamBase, teamId: str, db: Session=Depends(get_db)):
+    return crud.update_team(db=db, team_id=teamId, team=team)
+
+@app.post("/addTeam")
+def create_team(team: schemas.TeamBase, db: Session=Depends(get_db)):
+    return crud.create_team(db=db, team=team)
